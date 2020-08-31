5c2917ecf7851724a74817270d504a9145795caf
Usman Fayyaz <usman.fayyaz@ucp.edu.pk>
2020-08-31 11:32:00 +0500

    Updates

---
+++ precache-manifest.90b56fa9bafb6f46dd7d2d3301b8c0c2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aa12c55187d5d8fc5404b900fb96ca3c",
+    "url": "/online-quiz/index.html"
+  },
+  {
+    "revision": "e1aef14feae775a6c95f",
+    "url": "/online-quiz/static/css/main.68d88ce4.chunk.css"
+  },
+  {
+    "revision": "328ac2a0fcd71976cac1",
+    "url": "/online-quiz/static/js/2.2a295617.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/online-quiz/static/js/2.2a295617.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e1aef14feae775a6c95f",
+    "url": "/online-quiz/static/js/main.6c00476c.chunk.js"
+  },
+  {
+    "revision": "34e2129968b8b5a6b536",
+    "url": "/online-quiz/static/js/runtime-main.6b49364d.js"
+  },
+  {
+    "revision": "f2eb790fa27a4443847c0065de1bc235",
+    "url": "/online-quiz/static/media/questionImage.f2eb790f.png"
+  }
+]);
